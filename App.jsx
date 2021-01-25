0517cbec959b766a0c4f9843dbf7a2666a620c9f
shunJS <75880904+shunJS@users.noreply.github.com>
2021-01-25 23:14:24 +0900

    Replace keyborad with safeview

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen ';
 // import MemoListScreen from './src/screens/MemoListScreen';
 
@@ -9,7 +9,7 @@ export default function App() {
     // <MemoListScreen />
     // <MemoListScreen />
     // <MemoDetailScreen />
-    // <MemoEditScreen />
-    <MemoCreateScreen />
+    <MemoEditScreen />
+    // <MemoCreateScreen />
   );
 }
