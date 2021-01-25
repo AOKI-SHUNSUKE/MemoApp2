c3e40f4e37e45fb83acec4ac061bedc6a8977b82
shunJS <75880904+shunJS@users.noreply.github.com>
2021-01-25 14:03:49 +0900

    Exract components

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function AppBar() {
+  return (
+    <View style={styles.appbar}>
+      <View style={styles.appbarInner}>
+        <Text style={styles.appbarTitle}>Memo App</Text>
+        <Text style={styles.appbarRight}>logout</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appbar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: '#467FD3',
+    justifyContent: 'flex-end',
+  },
+  appbarInner: {
+    alignItems: 'center',
+  },
+  appbarRight: {
+    position: 'absolute',
+    right: 19,
+    bottom: 16,
+    color: 'rgba(255,255,255,0.8)',
+  },
+  appbarTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+  memoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0.15)',
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+});
